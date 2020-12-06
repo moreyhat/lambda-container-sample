f82f1a8784933aae18c22fd02e50918989fdac5a
moreyhat <moriyatto@gmail.com>
2020-12-06 21:55:12 +0900

    Add go sample

---
+++ go/app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name Event) (string, error) {
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
